47450df8449deb39f9caf82004c9ec4e90ad527c
AdrianPeterson <37534840+AdrianPeterson@users.noreply.github.com>
2021-11-04 19:35:40 +0800

    Update and rename solver.py to STAR_solver.py

--- STAR_solver.py
+++ STAR_solver.py
@@ -1,5 +1,5 @@
-from model import Generator
-from model import Discriminator
+from STAR_model import Generator
+from STAR_model import Discriminator
 from torch.autograd import Variable
 from torchvision.utils import save_image
 import torch
