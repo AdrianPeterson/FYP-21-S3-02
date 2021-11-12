3236d2c21208f66c1209ee63041fe31ff03aa6b9
AdrianPeterson <37534840+AdrianPeterson@users.noreply.github.com>
2021-11-12 21:38:20 +0800

    Update run.py

--- run.py
+++ run.py
@@ -39,43 +39,54 @@ if __name__ == "__main__":
     with open(opt.config) as f:
         config = yaml.load(f)
 
+    # Get checkpoint if available
     if opt.checkpoint is not None:
         log_dir = os.path.join(*os.path.split(opt.checkpoint)[:-1])
     else:
         log_dir = os.path.join(opt.log_dir, os.path.basename(opt.config).split('.')[0])
         log_dir += ' ' + strftime("%d_%m_%y_%H.%M.%S", gmtime())
 
+    # Create generator
     generator = OcclusionAwareGenerator(**config['model_params']['generator_params'],
                                         **config['model_params']['common_params'])
-
+    # Move generator to GPU if available
     if torch.cuda.is_available():
         generator.to(opt.device_ids[0])
     if opt.verbose:
         print(generator)
-
+    
+    # Create discriminator
     discriminator = MultiScaleDiscriminator(**config['model_params']['discriminator_params'],
                                             **config['model_params']['common_params'])
+    # Move discriminator to GPU if available
     if torch.cuda.is_available():
         discriminator.to(opt.device_ids[0])
     if opt.verbose:
         print(discriminator)
-
+    
+    # Create keypoint detector
     kp_detector = KPDetector(**config['model_params']['kp_detector_params'],
                              **config['model_params']['common_params'])
-
+    
+    # Move keypoint detector to GPU if available
     if torch.cuda.is_available():
         kp_detector.to(opt.device_ids[0])
 
     if opt.verbose:
         print(kp_detector)
 
+    # Create dataset if mode is "train"
+    # Since we are testing, no need to use
     dataset = FramesDataset(is_train=(opt.mode == 'train'), **config['dataset_params'])
 
+    # Create directory of logs
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
     if not os.path.exists(os.path.join(log_dir, os.path.basename(opt.config))):
         copy(opt.config, log_dir)
 
+    # Set mode of the model to either "train", "reconstruction" or animate
+    # For our case, we should set to "animate"
     if opt.mode == 'train':
         print("Training...")
         train(config, generator, discriminator, kp_detector, opt.checkpoint, log_dir, dataset, opt.device_ids)
